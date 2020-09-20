1c166109c99111567bbe41beac489fed2f2a63e0
MEETTEEM <382673663@qq.com>
2020-09-20 20:54:30 +0800

    Update kmp_count.cpp

--- kmp_count.cpp
+++ kmp_count.cpp
@@ -1,39 +1,38 @@
-void next() /// 处理模式串
+void next() /// 处理模式串的next数组
 {
 	int i , j ; i = 0 , j = nx[0] = -1 ;
 	while(i<m){
-		while(j!=-1 && pattern[i]!=pattern[j]) j = nx[j] ;
-		if(pattern[++i] == pattern[++j]) nx[i] =nx[j] ;
+		while(j!=-1 && sub[i]!=sub[j]) j = nx[j] ;
+		if(sub[++i] == sub[++j]) nx[i] =nx[j] ;
 		else nx[i] = j ;
 	}
 }
-int KMP()
+int KMP() /// 是否包含 sub 串
 {
 	int i = 0 , j = 0 ;
 	while(i<n && j<m){
-		if(primary[i]==pattern[j] || j==-1) {
+		if(str[i]==sub[j] || j==-1) {
 			++i , ++j ;
 			if(j==m) return i-m+1 ;
 		} else j = nx[j] ;
 	}
 	return -1 ;
 }
-int kmp_count()
+int kmp_count() /// 计算sub出现次数
 {
 	int i = 0 , j = 0 , ans = 0 ;
 	while(i<n){
 		while(j!=-1 && primary[i]!=pattern[j]) j = nx[j] ;
 		++i , ++j;
-		if(j>=m) ++ans , j = nx[j] ;
+		if(j>=m) ++ans , j = nx[j] ; /// 允许重叠
+		/// if(i>=m) ++ans , j = 0 ; /// 不允许重叠
 	}
 	return ans;
 }
-void solve() 
+int calc() /// 循环节
 {
-	for(cin>>t;t;t--){
-		memset(nx,-1,sizeof(nx));
-		cin>>pattern>>primary; n = primary.size() , m = pattern.size() ; /// 模式串，主串
-		next() ; cout<< KMP() <<endl ;
-		//// cout<<kmp_count()<<endl;
-	}
+	int m = str.size();
+	int len = m - nx[m] ; /// 循环节长度
+	if(m%len) cout<<len-m%len<<endl; /// 
+	else cout<<(len==m?m:0)<<endl; /// 
 }
